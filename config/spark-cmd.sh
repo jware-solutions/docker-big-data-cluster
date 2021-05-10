89a9cbf09fb873b4744565683322c84f4ae4bf2f
Genarito <genarocamele@hotmail.com>
2021-05-10 12:49:44 -0300

    + Added sleep to prevent some issues

--- config/spark-cmd.sh
+++ config/spark-cmd.sh
@@ -8,20 +8,23 @@ $HADOOP_HOME/sbin/start-yarn.sh
 sleep 5
 
 if [[ $1 = "start" ]]; then
-  if [[ $2 = "master-node" ]]; then
-    /sbin/spark-3.1.1-bin-without-hadoop/sbin/start-master.sh
+    if [[ $2 = "master-node" ]]; then
+        /sbin/spark-3.1.1-bin-without-hadoop/sbin/start-master.sh
+        sleep infinity
+        exit
+    fi
+    
+    # Sleeps to prevent connection issues with master
+    sleep 5
+    /sbin/spark-3.1.1-bin-without-hadoop/sbin/start-worker.sh master-node:7077
     sleep infinity
     exit
-  fi
-  /sbin/spark-3.1.1-bin-without-hadoop/sbin/start-worker.sh master-node:7077
-  sleep infinity
-  exit
 fi
 
 if [[ $1 = "stop" ]]; then
-  if [[ $2 = "master-node" ]]; then
-    /sbin/spark-3.1.1-bin-without-hadoop/sbin/stop-master.sh
-    exit
-  fi
-  /sbin/spark-3.1.1-bin-without-hadoop/sbin/stop-worker.sh
+    if [[ $2 = "master-node" ]]; then
+        /sbin/spark-3.1.1-bin-without-hadoop/sbin/stop-master.sh
+        exit
+    fi
+    /sbin/spark-3.1.1-bin-without-hadoop/sbin/stop-worker.sh
 fi
